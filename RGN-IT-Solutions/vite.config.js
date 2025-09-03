34373cf25a2658bf6f49b25e948054a96818e9fe
Gokulnath <rgokulnath24@gmail.com>
2025-09-03 08:28:27 +0530

    Changes made to Host

--- RGN-IT-Solutions/vite.config.js
+++ RGN-IT-Solutions/vite.config.js
@@ -15,4 +15,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/React-Projects/RGN-IT-Solutions/',   // 👈 important for GitHub Pages
 })
