930bc7ca64f2e699ee08ab1a2daa8060d61d1792
rgokulnath24 <rgokulnath24@gmail.com>
2026-08-30 12:33:04 +0530

    Add Tailwind CSS plugin to Vite configuration

--- Postify/vite.config.js
+++ Postify/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/React-Projects/postify/',   // 👈 important for GitHub Pages
-    plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
